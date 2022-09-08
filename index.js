f39835f576bfb07e681f2349cf6a8046468616f9
iammeskat <meskatcse@gmail.com>
2022-09-08 11:20:37 +0600

    add index file

---
+++ index.js
@@ -0,0 +1,8 @@
+var aws = require('aws-sdk');
+var nodemailer = require('nodemailer');
+const { spawnSync } = require("child_process");
+const fsExtra = require('fs-extra');
+
+exports.handler = (event, context, callback) => {
+    //
+}
